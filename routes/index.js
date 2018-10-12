7edcdace89f95b487b23cf73d08326cf7a5c978c
Antonio <antoniodaria@msn.com>
2018-10-12 12:37:24 +0100

    creating sign up and sign in views

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'MakersBnB' });
 });
 
+router.get('/signup', function(req, res) {
+  res.render('signup', { title: 'Add New User' });
+});
+
+// router.post('/', function(req, res, next) {
+//   // save to Database
+//   res.redirect('/profile', { title: 'Mfe' });
+// });
+
+
+router.get('/profile', function(req, res, next) {
+  res.render('profile', { title1: 'Mfe' });
+});
+
+
+
 module.exports = router;
